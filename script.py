66501e89f0e091ac2fb865d0528496849b676818
Likhon Sheikh <93394527+LikhonSheikhBot@users.noreply.github.com>
2021-11-10 02:28:06 +0600

    Add files via upload

---
+++ script.py
@@ -0,0 +1,25 @@
+class Script(object):
+
+    START_MSG = """<b>Hy {},
+
+I'm an advanced filter bot with many capabilities!
+There is no practical limits for my filtering capacity :)
+
+See <i>/help</i> for commands and more details.</b>
+"""
+
+
+    HELP_MSG = """
+How can i help you?
+"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : Likhon Sheikh</b>
+
+<b>Creater :</b> @GodFatherMob    
+
+<b>Language :</b> <code>Python3</code>
+
+<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+
+"""
